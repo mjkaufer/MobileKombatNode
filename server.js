c664e9c40188872367d6ed0b60d80af5aba4c017
pwstegman <pwstegman@gmail.com>
2017-10-31 22:39:00 -0500

    Cleaned coding style
    mix of tabs and spaces, mix of semicolons and no semicolons, it's madness, MADNESS

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 var express = require('express')
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 
 var app = express()
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
 
 server.listen(process.env.PORT || 3000)
 
 app.get('/', function(req, res){
-	//serve html page
+	// serve html page
 	// res.writeHead(200, {'Content-Type': 'text/html'})
 	res.sendFile(__dirname + '/static/index.html')
 })
 
 app.post('/webhook', function(req, res){
-	//handle webhook from twilio
-	
-	
-	var from = req.body.From;
-    var text = req.body.Body;
-    io.emit('cmd', text + "*" + from)
+	// handle webhook from twilio
+	var from = req.body.From
+	var text = req.body.Body
+	io.emit('cmd', text + "*" + from)
 	res.writeHead(200, {'Content-Type': 'text/html'})
 	res.end('Success!')
 })
 
-app.use(express.static('static'));
+app.use(express.static('static'))
